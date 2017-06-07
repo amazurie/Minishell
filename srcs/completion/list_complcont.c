dda170adc912752c0cbe971d92ea3493e1ae16c6
Admin <admin@Mac-Admin.local>
2017-06-07 19:47:36 +0200

    fix subfolder completion

--- srcs/completion/list_complcont.c
+++ srcs/completion/list_complcont.c
@@ -54,38 +54,78 @@ static int	r_complcont(t_arg **list, DIR *dirp, t_compl *c, char *pathcol)
 	return (check_compllist(list, listtmp));
 }
 
-static void	compl_cmp(t_compl *c, char *path, char **word, char **pathcol)
+static int	compl_cmp(t_compl *c, char *path, char **word, char *path2)
 {
-	if (ft_strcmp(path, *word) == 0)
+	int	i;
+	int	j;
+
+	j = 0;
+	if (ft_strcmp(path, *word) == 0 && (j = 1) > 0)
 	{
-		if ((*word)[0] == '.' && (!(*word)[1]
-				|| ((*word)[1] != '.' && !(*word)[2])))
+		if ((*word)[0] == '.' && (!(*word)[1] || ((*word)[1] != '.'
+					&& (*word)[1] != '/' && !(*word)[2])))
 			c->is_dot = 1;
-		*pathcol = ft_strdup(path);
 		if (c->is_dot || (*word)[ft_strlen(*word) - 1] == '/')
 			c->is_slash = 1;
-		if (!c->is_dot)
+		i = ft_strlen(*word) - 1;
+		while (i > 0 && (*word)[i] != '/' && (size_t)i > ft_strlen(path2) - 1)
+			i--;
+		if (!c->is_dot && (i <= 0 || (size_t)i == ft_strlen(*word) - 1))
 			ft_bzero(*word, ft_strlen(*word));
+		else if (!c->is_dot && ++i > 0 && (c->is_slash = 1) > 0)
+		{
+			ft_memmove(*word, (*word + i), ft_strlen((*word + i)));
+			ft_bzero((*word + ft_strlen((*word + i))), ft_strlen((*word + i)));
+		}
 	}
 	else if (ft_strcmp(path, ".") == 0)
-		*pathcol = ft_strdup(path);
-	if ((*word)[0] == '.')
-		c->is_dot = 1;
+		j = 1;
+	return (j);
 }
 
-t_arg		*list_content(t_compl *c, char *path, char *word)
+DIR			*check_open(char *firstpath, char **path)
+{
+	DIR		*dirp;
+	int		i;
+
+	if (!firstpath || !firstpath[0])
+		return (NULL);
+	if (!(*path = ft_strdup(firstpath)))
+		return (NULL);
+	if (!(dirp = opendir(*path)))
+	{
+		i = ft_strlen(*path) - 1;
+		while (i > 0 && (*path)[i] && (*path)[i] != '/')
+			i--;
+		ft_bzero((*path + i), ft_strlen(*path + i));
+		if (!*path || !(dirp = opendir(*path)))
+		{
+			if (*path)
+				free(*path);
+			return (NULL);
+		}
+	}
+	return (dirp);
+}
+
+t_arg		*list_content(t_compl *c, char *firstpath, char *word)
 {
 	DIR		*dirp;
 	t_arg	*list;
 	char	*pathcol;
+	char	*path;
 
-	if (!path || !(dirp = opendir(path)))
-		return (NULL);
 	pathcol = NULL;
-	compl_cmp(c, path, &word, &pathcol);
+	if (!(dirp = check_open(firstpath, &path)))
+		return (NULL);
+	if (compl_cmp(c, firstpath, &word, path))
+		pathcol = ft_strdup(path);
+	if (word[0] == '.')
+		c->is_dot = 1;
 	if (!(list = (t_arg *)ft_memalloc(sizeof(t_arg))))
 	{
 		closedir(dirp);
+		free(path);
 		return (NULL);
 	}
 	if (r_complcont(&list, dirp, c, pathcol) == 0)
@@ -93,5 +133,6 @@ t_arg		*list_content(t_compl *c, char *path, char *word)
 	closedir(dirp);
 	if (pathcol)
 		free(pathcol);
+	free(path);
 	return (list);
 }
